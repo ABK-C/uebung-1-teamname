4be16944069028ff89852b7a49f7931fd5d0ca5d
LukasEbeling <7lukas.e7@web.de>
2020-04-20 15:47:59 +0000

    Augabe 2

---
+++ summieren.cc
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+
+int main() {
+  std::ifstream dataIn("daten.txt");
+  std::ofstream dataOut("datensumme.txt");
+  for (int i; i < 234; ++i) {
+    int x;
+    int y;
+    dataIn >> x;
+    dataIn >> y;
+    std ::cout << x << " " << y << std ::endl;
+    int z = x + y;
+    std ::cout << z << std::endl;
+    dataOut << z;
+  }
+  dataOut.close();
+  dataIn.close();
+}
