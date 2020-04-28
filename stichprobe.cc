9a1520169e76d43dbdc07ff8477d837983c5ef47
LukasEbeling <7lukas.e7@web.de>
2020-04-28 15:24:01 +0000

    update

--- stichprobe.cc
+++ stichprobe.cc
@@ -1,10 +1,10 @@
-#include<fstream>
-#include<iostream>
-#include<cmath>
+#include <cmath>
+#include <fstream>
+#include <iostream>
 
-double mittelwert() {
+int main() {
+  // mittelwert
   std::ifstream dataIn("datensumme.txt");
-
   int summe = 0;
   int n = 0;
   while (!dataIn.eof()) {
@@ -13,36 +13,23 @@ double mittelwert() {
     dataIn >> a;
     summe = summe + a;
   }
-  --n;
-
   dataIn.close();
+  double mittelwert = summe / n;
+  std::cout << mittelwert << "\n";
 
-  double result = summe / n;
-  return result;
-}
-
-double varianz(double aritmittel) {
-  std::ifstream dataIn("datensumme.txt");
-
-  int summe = 0;
-  int n = 0;
-  while (!dataIn.eof()) {
-    ++n;
+  // abweichung
+  std::ifstream dataIn2("datensumme.txt");
+  int summe2 = 0;
+  while (!dataIn2.eof()) {
     int a = 0;
-    dataIn >> a;
-    int b = (a - aritmittel) * (a - aritmittel);
-    summe = summe + b;
+    dataIn2 >> a;
+    int b = (a - mittelwert) * (a - mittelwert);
+    summe2 = summe2 + b;
   }
-  --n;
+  dataIn2.close();
+  double abweichung = summe2 / n;
+  std::cout << abweichung << "\n";
 
-  dataIn.close();
-
-  double result = summe / n;
-  return result;
-}
-
-int main() {
-  std::cout << mittelwert() << "\n";
-  std::cout << varianz(mittelwert()) << "\n";
-  std::cout << sqrt(varianz(mittelwert())) << "\n";
+  // varianz
+  std::cout << sqrt(abweichung) << "\n";
 }
